0b209bf963f95c12627887745a41bf3c880d21ea
Matt Thrasher <matthew.j.thrasher@gmail.com>
2022-01-30 15:25:18 -0600

    Add user model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('yo');
+import { User } from './models/User';
+
+const user = new User({
+  name: 'dude',
+  age: 20,
+});
+
+user.on('change', () => {
+  console.log('im changing');
+});
+
+user.trigger('change');
+
+console.log(user);
