0d52e25d3df629800527aefa74e829b5e3d56ce2
Matt Thrasher <matthew.j.thrasher@gmail.com>
2022-02-01 13:02:35 -0600

    Add view with model and event  binding

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import { UserForm } from './views/UserForm';
 import { User } from './models/User';
 
-const collection = User.buildUserCollection();
+const user = User.buildUser({ name: 'NAME', age: 21 });
+const root = document.querySelector('#root');
 
-collection.on('change', () => {
-  console.log(collection);
-});
-collection.fetch();
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root must be present');
+}
