36fc2ac6188969f3053fc503dab95993015be438
Matt Thrasher <matthew.j.thrasher@gmail.com>
2022-02-01 15:44:49 -0600

    Add nested views

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'NAME', age: 21 });
 const root = document.querySelector('#root');
 
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+
+  console.log(userEdit);
 } else {
   throw new Error('Root must be present');
 }
